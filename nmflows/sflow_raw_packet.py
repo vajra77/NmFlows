b08e31b34e020ca4e92635debe71d0fd98937b12
vajra77 <f.ferreri@namex.it>
2023-10-19 09:42:36 +0200

    sketched application and basic sFlow packet parsing

--- nmflows/sflow_raw_packet.py
+++ nmflows/sflow_raw_packet.py
@@ -55,8 +55,7 @@ class SFlowRawPacket:
             raise Exception(f"sFlow version not supported: v{version}")
         ip_version = upx.unpack_uint()
         if ip_version == IP_VERSION_4:
-            #agent_address = socket.ntohl(upx.unpack_uint())
-            agent_address = socket.inet_ntoa(upx.unpack_fopaque(4))
+            agent_address = socket.inet_pton(socket.AF_INET, upx.unpack_fopaque(4))
         else:
             agent_address = socket.inet_ntop(socket.AF_INET6, upx.unpack_fopaque(16))
         agent_id = upx.unpack_uint()
