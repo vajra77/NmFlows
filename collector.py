e20ba8cb09de473004e419ddd7ec80bbf4ae7172
vajra77 <f.ferreri@namex.it>
2023-10-20 11:21:13 +0200

    guessing IPv4 header

--- collector.py
+++ collector.py
@@ -21,6 +21,8 @@ class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
         try:
             unpacker = xdrlib.Unpacker(data)
             datagram = create_sflow_datagram(unpacker)
+        except EOFError as e:
+            print("[ERROR] EOF while reading buffer")
         except Exception as e:
             print(f"[ERROR]: {e}")
         else:
