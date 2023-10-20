a5430fe68f6928f973596cce4383d386b54046fd
vajra77 <f.ferreri@namex.it>
2023-10-20 10:17:33 +0200

    guessing Ethernet header parsing

--- nmflows/ethernet_frame_header.py
+++ nmflows/ethernet_frame_header.py
@@ -35,12 +35,12 @@ class EthernetFrameHeader:
         # upx.unpack_fopaque(hdr_length - 14)
         dst_mac = ''.join('%02x' % b for b in upx.unpack_fopaque(6))
         src_mac = ''.join('%02x' % b for b in upx.unpack_fopaque(6))
-        eth_type = int.from_bytes(upx.unpack_fopaque(2), 'big')
+        eth_type = hex(int.from_bytes(upx.unpack_fopaque(2), 'big'))
         return cls(dst_mac, src_mac, eth_type, 14)
 
     def __repr__(self):
         return f"""
                                     Src MAC: {self.src_mac}
                                     Dst MAC: {self.dst_mac}
-                                    EthType: {self.type:%02x}
+                                    EthType: {self.type}
         """
