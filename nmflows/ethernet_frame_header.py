009396fa227aa62f394afccae280abbcf213f0c6
vajra77 <f.ferreri@namex.it>
2023-10-20 12:18:25 +0200

    guessing IPv4 header

--- nmflows/ethernet_frame_header.py
+++ nmflows/ethernet_frame_header.py
@@ -44,7 +44,7 @@ class EthernetFrameHeader:
         src_mac = ''.join('%02x' % b for b in upx.unpack_fopaque(6))
         eth_type = int.from_bytes(upx.unpack_fopaque(2), 'big')
 
-        if eth_type < 1500:
+        if eth_type < 1536:
             return cls(dst_mac, src_mac, 0, eth_type, 14)
         elif eth_type in ALLOWED_ETHERTYPES:
             if eth_type == ETHERTYPE_8021Q:
