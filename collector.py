812bfc004b04906383bdf9ab9e6771957e04f7ed
Francesco <fferreri@aries.namex.it>
2023-10-23 14:43:27 +0200

    refactoring server

--- collector.py
+++ collector.py
@@ -15,9 +15,7 @@ def create_sflow_datagram(upx: xdrlib.Unpacker):
 class ThisUDPRequestHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
-        data = self.rfile.readline().strip()
-        # socket = self.request[1]
-        # current_thread = threading.current_thread()
+        data = self.request[0]
         try:
             unpacker = xdrlib.Unpacker(data)
             datagram = create_sflow_datagram(unpacker)
