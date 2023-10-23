fb72f0a95e3253e91ed797bce59413e5f717fd7b
vajra77 <f.ferreri@namex.it>
2023-10-23 16:21:52 +0200

    added IPv6 parser

--- collector.py
+++ collector.py
@@ -15,7 +15,7 @@ def create_sflow_datagram(upx: xdrlib.Unpacker):
 class ThisUDPRequestHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
-        data = self.request[0].strip()
+        data = self.rfile.readline().strip()
         try:
             unpacker = xdrlib.Unpacker(data)
             datagram = create_sflow_datagram(unpacker)
