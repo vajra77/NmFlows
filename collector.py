5e9ce688e6fcd176f675a85fe0fb97acfd7e9ae9
vajra77 <f.ferreri@namex.it>
2023-10-23 15:32:02 +0200

    refactored threaded udp server

--- collector.py
+++ collector.py
@@ -15,7 +15,7 @@ def create_sflow_datagram(upx: xdrlib.Unpacker):
 class ThisUDPRequestHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
-        data = self.request[0]
+        data = self.request[0].strip()
         try:
             unpacker = xdrlib.Unpacker(data)
             datagram = create_sflow_datagram(unpacker)
