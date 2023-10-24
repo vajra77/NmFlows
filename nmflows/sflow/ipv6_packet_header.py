209f8dfb00e9cce1342e94c464904903ab3ee7dc
vajra77 <f.ferreri@namex.it>
2023-10-24 10:56:27 +0200

    implemented TCP/UDP parsing

--- nmflows/sflow/ipv6_packet_header.py
+++ nmflows/sflow/ipv6_packet_header.py
@@ -3,9 +3,10 @@ import socket
 
 class IPv6PacketHeader:
 
-    def __init__(self, src_addr, dst_addr, length):
+    def __init__(self, src_addr, dst_addr, proto, length):
         self._source_address = src_addr
         self._dest_address = dst_addr
+        self._proto = proto
         self._length = length
 
     @property
@@ -17,14 +18,19 @@ class IPv6PacketHeader:
         return self._dest_address
 
     @property
+    def proto(self):
+        return self._proto
+
+    @property
     def length(self):
         return self._length
 
     @classmethod
     def unpack(cls, data: bytes):
+        proto = int.from_bytes(data[6:7], 'big')
         src_addr = socket.inet_ntop(socket.AF_INET6, data[8:24])
         dst_addr = socket.inet_ntop(socket.AF_INET6, data[24:40])
-        return cls(src_addr, dst_addr, 40)
+        return cls(src_addr, dst_addr, proto, 40)
 
     def __repr__(self):
         return f"""
