082e4ab79e45afc9fc6c5002df01f073e8ec0516
vajra77 <f.ferreri@namex.it>
2023-10-24 11:30:59 +0200

    implemented TCP/UDP parsing

--- nmflows/sflow/udp_packet_header.py
+++ nmflows/sflow/udp_packet_header.py
@@ -2,9 +2,10 @@
 
 class UDPPacketHeader:
 
-    def __init__(self, src_port, dst_port):
+    def __init__(self, src_port, dst_port, p_len):
         self._source_port = src_port
         self._dest_port = dst_port
+        self._payload_length = p_len
 
     @property
     def source_port(self):
@@ -14,16 +15,22 @@ class UDPPacketHeader:
     def dest_port(self):
         return self._dest_port
 
+    @property
+    def payload_length(self):
+        return self._payload_length
+
     @classmethod
     def unpack(cls, data: bytes):
         src_port = int.from_bytes(data[0:2], 'big') & 0xffff
         dst_port = int.from_bytes(data[2:4], 'big') & 0xffff
-        return cls(src_port, dst_port)
+        payload_length = int.from_bytes(data[4:6], 'big') & 0xffff
+        return cls(src_port, dst_port, payload_length)
 
     def __repr__(self):
         return f"""
-                                        UDP Packet:
-                                            Src Port: {self.source_port}
-                                            Dst Port: {self.dest_port}
+                                UDP Packet:
+                                        Src Port: {self.source_port}
+                                        Dst Port: {self.dest_port}
+                                        Payload Len: {self.payload_length}
         """
 
