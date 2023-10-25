5f04af0600a571d875e95a7866666e01b712a040
vajra77 <f.ferreri@namex.it>
2023-10-25 08:10:27 +0200

    testing msg reception

--- broker.py
+++ broker.py
@@ -1,9 +1,15 @@
 from nmflows.mq import RecvQueue
+from nmflows.storage import StorableFlow
+import json
+import jsonpickle
 import sys
 
 
-def handle_msg():
-    pass
+def handle_msg(ch, method, properties, body):
+    flow = jsonpickle.decode(json.loads(body))
+    print(flow)
+
+
 
 if __name__ == "__main__":
     HOST = sys.argv[1]
