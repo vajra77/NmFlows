88f51b32a064becc644c006ae49dc288d5bed2c8
vajra77 <f.ferreri@namex.it>
2023-12-07 14:43:30 +0100

    optimized for performance

--- nmflows/mq/send_queue.py
+++ nmflows/mq/send_queue.py
@@ -7,13 +7,13 @@ class SendQueue(Queue):
     def __init__(self, host, port, name, user, passw):
         super().__init__(host, port, name, pika.PlainCredentials(user, passw))
         self._connection = pika.BlockingConnection(self._parameters)
-        self._channel = self._connection.channel()
-        self._channel.queue_declare(self._name)
 
     def send(self, msg):
         # connection = pika.BlockingConnection(self._parameters)
         # channel = connection.channel()
         # channel.queue_declare(self._name)
+        self._channel = self._connection.channel()
+        self._channel.queue_declare(self._name)
         self._channel.basic_publish(
             exchange='',
             routing_key=self._name,
