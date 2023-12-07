600738f3ab46cb9f879cd12e7f63bc300283a2de
vajra77 <f.ferreri@namex.it>
2023-12-07 13:50:56 +0100

    tuning rabbitmq connection heartbeat

--- nmflows/mq/send_queue.py
+++ nmflows/mq/send_queue.py
@@ -7,7 +7,11 @@ class SendQueue(Queue):
     def __init__(self, host, port, name, user, passw):
         super().__init__(host, port, name)
         credentials = pika.PlainCredentials(user, passw)
-        parameters = pika.ConnectionParameters(host, port, '/', credentials)
+        parameters = pika.ConnectionParameters(host=host, port=port,
+                                               virtual_host='/',
+                                               credentials=credentials,
+                                               heartbeat_interval=600,
+                                               blocked_connection_timeout=300)
         self._connection = pika.BlockingConnection(parameters)
         self._channel = self._connection.channel()
         self._channel.queue_declare(name)
