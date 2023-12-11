ef5bd924dc4e774c2a30b271d3d02881ac4cfb9e
vajra77 <f.ferreri@namex.it>
2023-12-11 15:45:10 +0100

    testing daemonization

--- broker.py
+++ broker.py
@@ -1,6 +1,8 @@
 from nmflows.peermatrix import PeeringMatrix
 from nmflows.mq import RecvQueue
 from config import CONFIG
+from daemonize import Daemonize
+import logging
 import json
 import jsonpickle
 import threading
@@ -31,13 +33,23 @@ def flush_task():
             Matrix.flush()
 
 
-if __name__ == '__main__':
-    try:
-        t1 = threading.Thread(target=consume_task)
-        t2 = threading.Thread(target=flush_task)
-        t1.start()
-        t2.start()
-        t1.join()
-        t2.join()
-    except KeyboardInterrupt:
-        exit()
+def do_main():
+    t1 = threading.Thread(target=consume_task)
+    t2 = threading.Thread(target=flush_task)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+if __name__ == "__main__":
+    pid = "/tmp/nmflows-broker.pid"
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+    fh = logging.FileHandler("/tmp/nmflows-broker.log", "w")
+    fh.setLevel(logging.DEBUG)
+    logger.addHandler(fh)
+    keep_fds = [fh.stream.fileno()]
+
+    daemon = Daemonize(app="nmflows-broker", pid=pid, action=do_main, keep_fds=keep_fds)
+    daemon.start()
