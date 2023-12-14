52b34fc57d8b67f2964aa46e4518016c88dfb1e3
vajra77 <f.ferreri@namex.it>
2023-12-14 11:23:25 +0100

    fixed bug with bytes count

--- nmflows/peermatrix/peering_matrix.py
+++ nmflows/peermatrix/peering_matrix.py
@@ -92,8 +92,8 @@ class PeeringMatrix:
             tot4 = 0
             tot6 = 0
             for dst in src.destinations:
-                msg += f"{dst.name}[{dst.mac}]=({dst.ipv4_bytes}/{dst.ipv6_bytes}) "
-                tot4 += dst.ipv4_bytes
-                tot6 += dst.ipv6_bytes
-            msg += f"| SUM({tot4}/{tot6}) TOT({src.ipv4_bytes}/{src.ipv6_bytes})\n"
+                msg += f"{dst.name}[{dst.mac}]=({dst.ipv4_in_bytes}/{dst.ipv6_in_bytes}) "
+                tot4 += dst.ipv4_in_bytes
+                tot6 += dst.ipv6_in_bytes
+            msg += f"| SUM({tot4}/{tot6}) TOT({src.ipv4_out_bytes}/{src.ipv6_out_bytes})\n"
         return msg    
