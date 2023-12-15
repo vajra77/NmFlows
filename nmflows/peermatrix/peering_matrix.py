b4584dc9f666959d99568806c6e57da9fcbde13e
vajra77 <f.ferreri@namex.it>
2023-12-15 10:01:54 +0100

    improved matrix dump

--- nmflows/peermatrix/peering_matrix.py
+++ nmflows/peermatrix/peering_matrix.py
@@ -93,7 +93,7 @@ class PeeringMatrix:
             tot6 = 0
             for dst in src.destinations:
                 # msg += f"{dst.name}[{dst.mac}]=({dst.ipv4_in_bytes}/{dst.ipv6_in_bytes}) "
-                msg += f"{dst.name}[:{dst.mac[:-2]}] | "
+                msg += f"{dst.name}[:{dst.mac[-2:]}] | "
                 tot4 += dst.ipv4_in_bytes
                 tot6 += dst.ipv6_in_bytes
             # msg += f"| SUM({tot4}/{tot6}) TOT({src.ipv4_out_bytes}/{src.ipv6_out_bytes})\n"
