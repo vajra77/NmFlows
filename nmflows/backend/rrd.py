3a3ffd9b674dc8bda8ed9c0dafa6b202233ceafc
vajra77 <f.ferreri@namex.it>
2023-12-20 12:42:45 +0100

    adding support for Retool interface

--- nmflows/backend/rrd.py
+++ nmflows/backend/rrd.py
@@ -116,7 +116,7 @@ class RRDBackend(Backend):
             os.unlink(imgfile)
             return data
         else:
-            f = open('/srv/NmFlows/static/404.png')
+            f = open('static/404.png', mode='rb')
             data = f.read()
             f.close()
             return data
@@ -158,7 +158,7 @@ class RRDBackend(Backend):
             os.unlink(imgfile)
             return data
         else:
-            f = open('/srv/NmFlows/static/404.png')
+            f = open('static/404.png', mode='rb')
             data = f.read()
             f.close()
             return data
