b1839d94bb1630ed2990584bb03292d448a62fab
vajra77 <f.ferreri@namex.it>
2023-12-21 16:05:34 +0100

    troubleshooting

--- broker.py
+++ broker.py
@@ -76,6 +76,5 @@ if __name__ == "__main__":
                       CONFIG['rabbitmq_pass'],
                       handle_msg)
 
-    do_main()
-    #daemon = Daemonize(app="nmflows-broker", pid=CONFIG['broker_pid'], action=do_main, keep_fds=Stats.keep_fds)
-    #daemon.start()
+    daemon = Daemonize(app="nmflows-broker", pid=CONFIG['broker_pid'], action=do_main, keep_fds=Stats.keep_fds)
+    daemon.start()
